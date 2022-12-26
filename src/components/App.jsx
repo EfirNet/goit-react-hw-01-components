2157852a72981e9738540315a2f32b5ae1212893
efirnet <efirnet@gmail.com>
2022-12-26 23:11:02 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+
+import user from '../json/user.json'
+import data from '../json/data.json'
+import friends from '../json/friends.json'
+import transactions from '../json/transactions.json'
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <div
+        style={{
+          marginTop: '10px',
+          height: '100vh',
+          display: 'flex',
+          flexWrap: 'wrap',
+          // justifyContent: 'space-evenly',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+        <Statistics title="Upload stats" stats={data} />
+        <Statistics stats={data} />
+        <FriendList friends={friends} />
+        <TransactionHistory items={transactions} />
+
+      </div>
+    </>
   );
 };
