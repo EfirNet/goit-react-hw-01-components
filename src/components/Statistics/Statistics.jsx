be7301f6123a72264840b7f1c03fa351ead286da
efirnet <efirnet@gmail.com>
2022-12-27 11:04:20 +0200

    fix- del default title

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,7 +5,7 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-export const Statistics = ({ title = 'Upload stats', stats }) => {
+export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
       {title && <h2 className={css.title}>{title}</h2>}
